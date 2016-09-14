d4450045799791ddc9f30b5b970f22171e8afc5b
paly <jirachot23@gmail.com>
2016-09-14 10:54:57 +0700

    Test

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ch1</title>
+    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container" style="margin-top:20px;">
+      <div class="panel panel-default">
+        <div class="panel-body">
+          Container
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid" style="margin-top:20px;">
+      <div class="panel panel-default">
+        <div class="panel-body">
+          Container-Fluid
+        </div>
+      </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
